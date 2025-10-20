58700804edef5bc84b31fecbf814e2db8c907ff4
userName <userEmail>
2025-10-20 17:06:44 +0600

    added

--- app/layout.js
+++ app/layout.js
@@ -2,7 +2,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Nav from "./component/Nav";
 import Top from "./component/Top";
-import Splash from "./component/Splash";
+// import Splash from "./component/Splash";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
